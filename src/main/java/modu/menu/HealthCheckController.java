96c89b1d33c54b9ed4f8ea1f4327ec42b6caee8d
eelseungmin <berrypicker777@gmail.com>
2024-01-15 15:36:19 +0900

    feat: getResponseTest api 추가

--- src/main/java/modu/menu/HealthCheckController.java
+++ src/main/java/modu/menu/HealthCheckController.java
@@ -1,5 +1,7 @@
 package modu.menu;
 
+import modu.menu.core.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class HealthCheckController {
     @GetMapping("/api/health-check")
     public String getHealthCheck() {
         return "up";
+    }
+
+    @GetMapping("/api/test")
+    public ResponseEntity<?> getResponseTest() {
+        return ResponseEntity.ok().body(new ApiResponse<>("test"));
     }
 }
