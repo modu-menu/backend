245aab3b6cec8361b8b85b54ea878ed7db9bbbc9
eelseungmin <berrypicker777@gmail.com>
2024-01-24 20:31:46 +0900

    fix: 테스트시 발생하는 에러 해결을 위해 JpaAuditingConfig 클래스 분리

--- src/main/java/modu/menu/ModuMenuApplication.java
+++ src/main/java/modu/menu/ModuMenuApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class ModuMenuApplication {
 
