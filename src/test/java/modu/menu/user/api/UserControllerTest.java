3d4b8efcc37b2cdaee8228816d36937f8668806c
eelseungmin <berrypicker777@gmail.com>
2024-01-28 19:16:39 +0900

    test: tempLogin(로그인) controller test 작성

--- src/test/java/modu/menu/user/api/UserControllerTest.java
+++ src/test/java/modu/menu/user/api/UserControllerTest.java
@@ -1,15 +1,20 @@
 package modu.menu.user.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import modu.menu.ControllerTestComponent;
 import modu.menu.user.api.request.TempJoinRequest;
+import modu.menu.user.api.request.TempLoginRequest;
 import modu.menu.user.service.dto.TempJoinResultDto;
+import modu.menu.user.service.dto.TempLoginResultDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -44,5 +49,32 @@ class UserControllerTest extends ControllerTestComponent {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(tempJoinRequest)))
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("등록된 회원으로 로그인한다.")
+    @Test
+    void login() throws Exception {
+        // given
+        TempLoginRequest tempLoginRequest = TempLoginRequest.builder()
+                .email("test1234@naver.com")
+                .password("test1234")
+                .build();
+
+        // when
+        when(userService.tempLogin(any()))
+                .thenReturn(TempLoginResultDto.builder()
+                        .id(1L)
+                        .name("홍길동")
+                        .nickname("test1234")
+                        .profileImageUrl("")
+                        .accessToken("token")
+                        .build());
+
+        // then
+        mockMvc.perform(post("/api/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tempLoginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Authorization", "token"));
     }
 }
