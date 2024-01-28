9eff5c90324725615e207c0dcf5f19c23b36f2eb
eelseungmin <berrypicker777@gmail.com>
2024-01-28 19:29:40 +0900

    test: tempLogin(로그인) service test 작성

--- src/test/java/modu/menu/user/service/UserServiceTest.java
+++ src/test/java/modu/menu/user/service/UserServiceTest.java
@@ -1,11 +1,13 @@
 package modu.menu.user.service;
 
 import modu.menu.user.api.request.TempJoinRequest;
+import modu.menu.user.api.request.TempLoginRequest;
 import modu.menu.user.domain.Gender;
 import modu.menu.user.domain.User;
 import modu.menu.user.domain.UserStatus;
 import modu.menu.user.repository.UserRepository;
 import modu.menu.user.service.dto.TempJoinResultDto;
+import modu.menu.user.service.dto.TempLoginResultDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -56,16 +58,34 @@ class UserServiceTest {
         verify(userRepository, times(1)).save(any(User.class));
     }
 
+    @DisplayName("회원의 이메일과 비밀번호를 입력하면, 해당 회원의 데이터를 반환한다.")
+    @Test
+    void tempLogin() {
+        // given
+        TempLoginRequest tempLoginRequest = TempLoginRequest.builder()
+                .email("test@naver.com")
+                .password("test1234")
+                .build();
+        User tempUser = createUser(1L, "test@naver.com");
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(tempUser));
+
+        // when
+        TempLoginResultDto tempLoginResultDto = userService.tempLogin(tempLoginRequest);
+
+        // then
+        verify(userRepository, times(1)).findByEmail(anyString());
+    }
+
     private User createUser(Long id, String email) {
         return User.builder()
                 .id(id)
                 .email(email)
-                .name("name" + id)
-                .nickname("nickname" + id)
+                .name("name")
+                .nickname("nickname")
                 .gender(Gender.MALE)
                 .age(id.intValue())
                 .birthday(LocalDate.now())
-                .password("password" + id)
+                .password("test1234")
                 .phoneNumber("01012345678")
                 .status(UserStatus.ACTIVE)
                 .build();
