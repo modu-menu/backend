8cce99038c4d3138dcc286af0932db7ece584f16
eelseungmin <berrypicker777@gmail.com>
2024-03-02 20:21:13 +0900

    fix: EnumValidation 수정에 맞춰 기존 Placecontroller 코드 수정

--- src/main/java/modu/menu/place/api/PlaceController.java
+++ src/main/java/modu/menu/place/api/PlaceController.java
@@ -2,10 +2,10 @@ package modu.menu.place.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import modu.menu.core.annotation.EnumValidation;
 import modu.menu.core.response.ApiSuccessResponse;
 import modu.menu.food.domain.FoodType;
 import modu.menu.place.api.response.SearchPlaceResponse;
@@ -30,14 +30,23 @@ public class PlaceController {
     @Operation(summary = "음식점 후보 검색", description = "투표에 포함시킬 음식점 후보를 사용자의 입력 값을 바탕으로 검색합니다.")
     @GetMapping("/api/place")
     public ResponseEntity<ApiSuccessResponse<SearchPlaceResponse>> searchPlace(
-            @NotNull(message = "위도는 필수입니다.") @PositiveOrZero(message = "위도는 0 또는 양수여야 합니다.") @RequestParam Double latitude,
-            @NotNull(message = "경도는 필수입니다.") @PositiveOrZero(message = "경도는 0 또는 양수여야 합니다.") @RequestParam Double longitude,
-            @Size(min = 1, message = "음식은 입력 시 최소 1개의 값이 필요합니다.") @RequestParam List<FoodType> food,
-            @Size(min = 1, message = "분위기는 입력 시 최소 1개의 값이 필요합니다.") @RequestParam List<VibeType> vibe,
-            @NotNull(message = "페이지 번호는 필수입니다.") @PositiveOrZero(message = "페이지 번호는 0 또는 양수여야 합니다.") @RequestParam(defaultValue = "0") Integer page
+            @PositiveOrZero(message = "위도는 0 또는 양수여야 합니다.") @RequestParam(defaultValue = "37.505098") Double latitude,
+            @PositiveOrZero(message = "경도는 0 또는 양수여야 합니다.") @RequestParam(defaultValue = "127.032941") Double longitude,
+            @EnumValidation(enumClass = FoodType.class, message = "음식에 허용된 값만 입력해주세요.") @Size(min = 1, message = "음식은 입력 시 최소 1개의 값이 필요합니다.")
+            @RequestParam(required = false) List<String> food,
+            @EnumValidation(enumClass = VibeType.class, message = "분위기에 허용된 값만 입력해주세요.") @Size(min = 1, message = "분위기는 입력 시 최소 1개의 값이 필요합니다.")
+            @RequestParam(required = false) List<String> vibe,
+            @PositiveOrZero(message = "페이지 번호는 0 또는 양수여야 합니다.") @RequestParam(defaultValue = "0") Integer page
     ) {
-        SearchPlaceResponse searchPlaceResponse = placeService.searchPlace(latitude, longitude, food, vibe, page);
+        SearchPlaceResponse searchPlaceResponse = placeService.searchPlace(
+                latitude,
+                longitude,
+                food != null ? food.stream().map(f -> FoodType.valueOf(f.toUpperCase())).toList() : null,
+                vibe != null ? vibe.stream().map(v -> VibeType.valueOf(v.toUpperCase())).toList() : null,
+                page
+        );
 
-        return ResponseEntity.ok(new ApiSuccessResponse<>(searchPlaceResponse));
+        return ResponseEntity.ok()
+                .body(new ApiSuccessResponse<>(searchPlaceResponse));
     }
 }
