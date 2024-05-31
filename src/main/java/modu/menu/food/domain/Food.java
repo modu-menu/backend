68a09b9ca1466da423cb5090f8c2230c64fdd499
eelseungmin <berrypicker777@gmail.com>
2024-05-31 23:24:41 +0900

    refactor: 테이블에서 계층을 관리하지 않도록 변경함에 따라 엔티티 구조를 이전과 같이 복구

--- src/main/java/modu/menu/food/domain/Food.java
+++ src/main/java/modu/menu/food/domain/Food.java
@@ -3,9 +3,6 @@ package modu.menu.food.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,26 +18,4 @@ public class Food {
     @Column(length = 30)
     @Enumerated(EnumType.STRING)
     private FoodType type;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "food_id")
-    private Food parent;
-
-    @Builder.Default
-    @OneToMany
-    private List<Food> children = new ArrayList<>();
-
-    public void addChildren(Food food) {
-        food.syncParent(this);
-        children.add(food);
-    }
-
-    public void removeChildren(Food food) {
-        food.syncParent(null);
-        children.remove(food);
-    }
-
-    public void syncParent(Food food) {
-        this.parent = food;
-    }
 }
